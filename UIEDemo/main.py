046d5f372d3a51b5d8233f1969a765564fef5e2b
rly <rly@evecom.net>
2022-08-18 19:26:23 +0800

    修改schema 测试

--- UIEDemo/main.py
+++ UIEDemo/main.py
@@ -13,8 +13,7 @@ def legal_judgement_ie(content):
     print(pformat(ie(content)))
 
 def drug_ie(content):
-    schema = ['地点', '时间', '毒品重量', {'涉案人': '涉案人'},
-              {'涉案人': '毒品类型'}]  # Define the schema for entity extraction
+    schema = ['地点', '时间', '毒品重量', {'涉案人': ['涉案人', '涉案人', '毒品类型']}]  # Define the schema for entity extraction
     ie = Taskflow('information_extraction', schema=schema, task_path='./drug_checkpoint/model_best')
     pprint(content)
     pprint(ie(content))  # Better print results using pprint
@@ -28,21 +27,21 @@ def test_ie(content):
 
 
 if __name__ == '__main__':
-    legal_judgement_ie("上海金融法院"
-                       "民事判决书"
-                       "案号：（2022）沪74民终186号"
-                       "上诉人（原审被告）：潢川县发展投资有限责任公司，住所地河南省信阳市潢川县财政局。"
-                       "法定代表人：梅利平，董事长。"
-                       "委托诉讼代理人：李明修，该公司员工。"
-                       "委托诉讼代理人：李阳，河南捷达律师事务所律师。"
-                       "被上诉人（原审原告）：海发宝诚融资租赁有限公司（原名中远海运租赁有限公司），住所地中国（上海）自由贸易试验区福山路450号3E室。"
-                       "法定代表人：陈易明，总经理。"
-                       "委托诉讼代理人：邹华恩，北京德和衡（上海）律师事务所律师。"
-                       "委托诉讼代理人：王坤，北京德和衡（上海）律师事务所律师。")
-    # with open("./data/CAIL2022_ie/step1_test.json", "r", encoding="utf-8") as f:
-    #     raw_examples = f.readlines()
-    # line = raw_examples[92]
-    # drug_ie(line)
+    # legal_judgement_ie("上海金融法院"
+    #                    "民事判决书"
+    #                    "案号：（2022）沪74民终186号"
+    #                    "上诉人（原审被告）：潢川县发展投资有限责任公司，住所地河南省信阳市潢川县财政局。"
+    #                    "法定代表人：梅利平，董事长。"
+    #                    "委托诉讼代理人：李明修，该公司员工。"
+    #                    "委托诉讼代理人：李阳，河南捷达律师事务所律师。"
+    #                    "被上诉人（原审原告）：海发宝诚融资租赁有限公司（原名中远海运租赁有限公司），住所地中国（上海）自由贸易试验区福山路450号3E室。"
+    #                    "法定代表人：陈易明，总经理。"
+    #                    "委托诉讼代理人：邹华恩，北京德和衡（上海）律师事务所律师。"
+    #                    "委托诉讼代理人：王坤，北京德和衡（上海）律师事务所律师。")
+    with open("./data/CAIL2022_ie/step1_test.json", "r", encoding="utf-8") as f:
+        raw_examples = f.readlines()
+    line = raw_examples[92]
+    drug_ie(line)
     # test_ie("这也让很多业主据此认为，雅清苑是政府公务员挤对了国家的经适房政策。")
 
 # 上海金融法院 民事判决书 案号：（2022）沪74民终186号 上诉人（原审被告）：潢川县发展投资有限责任公司，住所地河南省信阳市潢川县财政局。 法定代表人：梅利平，董事长。 委托诉讼代理人：李明修，该公司员工。 委托诉讼代理人：李阳，河南捷达律师事务所律师。 被上诉人（原审原告）：海发宝诚融资租赁有限公司（原名中远海运租赁有限公司），住所地中国（上海）自由贸易试验区福山路450号3E室。 法定代表人：陈易明，总经理。 委托诉讼代理人：邹华恩，北京德和衡（上海）律师事务所律师。 委托诉讼代理人：王坤，北京德和衡（上海）律师事务所律师。
